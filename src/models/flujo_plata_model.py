caecb1f6fa4b58693e7242ab7189e5355bef6e8f
jero925 <mygameoption@gmail.com>
2024-05-01 21:42:46 -0300

    WIP Class Databases

--- src/models/flujo_plata_model.py
+++ src/models/flujo_plata_model.py
@@ -1,4 +1,10 @@
-"""Model para la base de datos cuotas"""
+"""
+    La idea es la siguiente: tener una class padre Databases, donde voy a crear una nueva
+    class hija por cada DB que haya.
+    En la padre, va a estar como metodos los comunes, get, create, etc.
+    Los atributos icon, parent, name. El resto de atributos van a ser propios de cada class hija
+"""
+
 class FlujoPlata():
     """Corresponde a los datos de la DB Cuotas"""
     def __init__(self, parent, name, total_price, dues_qty, date_first_due, buy_due, months) -> None:
@@ -11,7 +17,7 @@ class FlujoPlata():
         self.buy_due = buy_due
         self.months = months
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         """Transforma clase a dict"""
         return {
             "icon": self.icon,
