bcf37fc0d7d2c564b073ee2695c4db34cf83930b
jero925 <mygameoption@gmail.com>
2024-05-06 18:53:27 -0300

    Add FlujoPlata db Model

--- src/models/flujo_plata.py
+++ src/models/flujo_plata.py
@@ -1,38 +1,108 @@
-"""
-    La idea es la siguiente: tener una class padre Databases, donde voy a crear una nueva
-    class hija por cada DB que haya.
-    En la padre, va a estar como metodos los comunes, get, create, etc.
-    Los atributos icon, parent, name. El resto de atributos van a ser propios de cada class hija
-"""
+"""Model para la base de datos Flujo_Plata"""
+import os
+from .database_model import SpecificDatabase
+class FlujoPlata(SpecificDatabase):
+    """
+    Inicializa una instancia de Cuota.
+    """
 
-class FlujoPlata():
-    """Corresponde a los datos de la DB Cuotas"""
-    def __init__(self,
-                 parent,
-                 name,
-                 total_price,
-                 dues_qty,
-                 date_first_due,
-                 buy_due,
-                 months) -> None:
-        self.icon = "https://www.notion.so/icons/credit-card_gray.svg"
-        self.parent = parent
-        self.name = name
-        self.total_price = total_price
-        self.dues_qty = dues_qty
-        self.date_first_due = date_first_due
-        self.buy_due = buy_due
-        self.months = months
-
-    def to_dict(self) -> dict:
-        """Transforma clase a dict"""
-        return {
-            "icon": self.icon,
-            "parent": self.parent,
-            "Name": self.name,
-            "Monto Total": self.total_price,
-            "Cantidad de cuotas": self.dues_qty,
-            "Primer cuota": self.date_first_due,
-            "Fecha de compra": self.buy_due,
-            "Meses": self.months
+    def __init__(self) -> None:
+        self.database_id: str = os.getenv("FLUJOPLATA_DB_ID")
+        super().__init__(database_id=self.database_id)
+        self.icon: str = ""
+        self.properties: dict = {
+            "Fecha": {
+                "type": "date",
+                "date": {
+                        "start": "2024-01-04"
+                }
+            },
+            "Producto en cuotas": {
+                "type": "relation",
+                "relation": [],
+            },
+            "Cuenta": {
+                "type": "relation",
+                "relation": [],
+            },
+            "Tipo": {
+                "type": "multi_select",
+                "multi_select": []
+            },
+            "Suscripcion": {
+                "type": "relation",
+                "relation": [],
+            },
+            "I/O": {
+                "type": "select",
+                "select": {
+                        "name": "Gasto"
+                }
+            },
+            "Estado Suscripcion": {
+                "type": "status",
+                "status": {
+                        "name": "No sub"
+                }
+            },
+            "Ingreso. Mes Año": {
+                "type": "relation",
+                "relation": [],
+            },
+            "Monto": {
+                "type": "number",
+                "number": 1
+            },
+            "Gasto. Mes Año": {
+                "type": "relation",
+                "relation": [],
+            },
+            "Nombre": {
+                "id": "title",
+                "type": "title",
+                "title": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": ""
+                            }
+                        }
+                ]
+            }
         }
+
+def create_db_page(flujo_plata: FlujoPlata) -> None:
+    """Ejemplo de creacion de pagina en DB Cuotas"""
+    database_props_modified: dict = {
+        "icon": "https://www.notion.so/icons/credit-card_gray.svg",
+        "parent": "",
+        "Nombre": "Prueba flujo plata",
+        "Monto": 123456,
+        "I/O": "Gasto",
+        "Fecha": "2024-11-15",
+        "Cuenta": [],
+        "Gasto. Mes Año": ["d9c435da42a445b48ceaf181a5615380"],
+        "Tipo": ["Sueldo", "Suscripcion"]
+    }
+
+    # print(database_props_modified)
+    flujo_plata.create_page(props_modified=database_props_modified)
+
+def main() -> None:
+    """
+    Función principal para demostrar el uso de la clase Database.
+    """
+    flujo_plata_database = FlujoPlata()
+
+    filtros: dict = {"property": "Activa","checkbox": {"equals": True}}
+
+    create_db_page(flujo_plata_database)
+
+    # query_database_results = flujo_plata_database.query_database()
+    # print(query_database_results)
+
+    # names_rows_results: dict = flujo_plata_database.get_titles_rows_db()
+    # print(names_rows_results)
+
+if __name__ == "__main__":
+    main()
